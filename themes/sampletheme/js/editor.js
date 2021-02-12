9fadea3e450e3e1a13be92b60e9918d91b07c6be
Mausami Desai <mausamidesai06@gmail.com>
2021-02-11 20:25:07 -0500

    Do some changes according to class example

---
+++ themes/sampletheme/js/editor.js
@@ -0,0 +1,22 @@
+wp.blocks.registerBlockStyle( 
+    'core/quote', 
+    {
+        name: 'fancy-quote',
+        label: 'Fancy Quote'
+    } 
+);
+
+wp.blocks.registerBlockStyle( 
+    'core/button', 
+    {
+        name: 'fancy-button',
+        label: 'Fancy Button'
+    } 
+);
+
+//wp.blocks.unregisterBlockStyle( 'core/quote', 'fancy-quote' );
+
+wp.domReady( function() {
+//    wp.blocks.unregisterBlockStyle( 'core/quote', 'default' );
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+} );
