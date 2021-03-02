fab157b872b291930bfbfd3ca3921c7ef13904ea
Mausami Desai <mausamidesai06@gmail.com>
2021-03-02 02:36:30 -0500

    Did coding for email

--- themes/sampletheme/inc/woocommerce.php
+++ themes/sampletheme/inc/woocommerce.php
@@ -6,11 +6,11 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 0 );
 
 function sampletheme_woocommerce_single_product_summary_subtitle() {
-    echo '<b style="color: yellow;">Look at the amazing price!!</b>';
+    echo '<b style="color: #f9b51f;">Look at the amazing price!!</b>';
 }
 add_action( 'woocommerce_single_product_summary', 'sampletheme_woocommerce_single_product_summary_subtitle', 2 );
 
 function sampletheme_woocommerce_before_add_to_cart_quantity_test() {
-    echo '<p>TEST</p>';
+    
 }
 add_action( 'woocommerce_before_add_to_cart_quantity', 'sampletheme_woocommerce_before_add_to_cart_quantity_test', 5 );
