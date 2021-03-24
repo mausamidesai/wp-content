b905c9cbfe2818d5329ed52a3d7a8c4306d94eb8
Mausami Desai <mausamidesai06@gmail.com>
2021-03-23 21:43:22 -0400

    Implemented slider to the sample theme site

--- themes/sampletheme/footer.php
+++ themes/sampletheme/footer.php
@@ -11,6 +11,25 @@
 
 ?>
 </div>
+
+<?php
+$recipe_args = array(
+	'post_type' => 'sampletheme_recipe',
+	'posts_per_page' => 3,
+);
+
+$recipe_query = new WP_Query( $recipe_args );
+
+if ( $recipe_query->have_posts() ) {
+	while ( $recipe_query->have_posts() ) {
+		$recipe_query->the_post();
+		?>
+		<h2><?php the_title(); ?></h2>
+		<?php
+	}
+}
+?>
+
 	<footer id="colophon" class="site-footer">
 		<div class="grid-container">
 
